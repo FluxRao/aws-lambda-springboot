b72ae1783541bf4adb2bcb095cdb748050cca697
FluxRao29 <90955407+FluxRao29@users.noreply.github.com>
2023-07-22 01:46:23 +0530

    Error resolved | returns the response now

---
+++ src/main/java/com/flux/aws/lambda/OrderHandler.java
@@ -0,0 +1,6 @@
+package com.flux.aws.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String,Object> {
+}
